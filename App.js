c967b0ed2d10187166c198c33f297b8e723039cc
kenny <adelakunmoyok@gmail.com>
2026-01-04 23:35:19 +0100

    commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { Text } from 'react-native/types_generated/index';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,6 +15,7 @@ function App() {
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
+      <Text> Text </Text>
     </SafeAreaProvider>
   );
 }
