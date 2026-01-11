76b47c94c5b67e80c1706506eea0ed165d0ae093
kenny <adelakunmoyok@gmail.com>
2026-01-11 17:57:15 +0100

    commit

--- App.js
+++ App.js
@@ -1,42 +1,23 @@
 
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-import { Text } from 'react-native/types_generated/index';
+// import { NewAppScreen } from '@react-native/new-app-screen';
+import { Text, } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+// import { Text } from 'react-native/types_generated/index';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+    <SafeAreaView>
       <Text> Text </Text>
-    </SafeAreaProvider>
+    </SafeAreaView>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
 
 export default App;
