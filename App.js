36e9f7d9851ddf8a0216ee0072470ca357c88856
kenny <adelakunmoyok@gmail.com>
2026-01-15 22:06:22 +0100

    comit

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
-
-
-
-import { Text, } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 // import { Text } from 'react-native/types_generated/index';
 
 function App() {
-
   return (
-    <SafeAreaView>
-      <Text> Hello World 👋 🌍! </Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}> Hello World 👋 🌍! </Text>
     </SafeAreaView>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    // alignSelf: 'center',
+
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
